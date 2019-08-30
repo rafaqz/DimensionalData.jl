9e6490155eb27a8df28b9de0e37bf062015cc09b
Rafael Schouten <rafaelschouten@gmail.com>
2019-08-30 17:17:53 +1000

    more eachslice

--- test/runtests.jl
+++ test/runtests.jl
@@ -212,17 +212,19 @@ ms = mapslices(sum, da; dims=Time)
 
 # Iteration methods
 
-# eachslice
-da = DimensionalArray(a, (Y(10:30), Time(1:4)))
-@test [mean(s) for s in eachslice(da; dims=Time)] == [3.0, 4.0, 5.0, 6.0]
-slices = [s .* 2 for s in eachslice(da; dims=Y)] 
-@test slices[1] == [2, 4, 6, 8]
-@test slices[2] == [6, 8, 10, 12]
-@test slices[3] == [10, 12, 14, 16]
-dims(slices[1]) == (Time(1.0:1.0:4.0),)
-slices = [s .* 2 for s in eachslice(da; dims=Time)] 
-@test slices[1] == [2, 6, 10]
-dims(slices[1]) == (Y(10.0:10.0:30.0),)
+if VERSION > v"1.1-"
+    # eachslice
+    da = DimensionalArray(a, (Y(10:30), Time(1:4)))
+    @test [mean(s) for s in eachslice(da; dims=Time)] == [3.0, 4.0, 5.0, 6.0]
+    slices = [s .* 2 for s in eachslice(da; dims=Y)] 
+    @test slices[1] == [2, 4, 6, 8]
+    @test slices[2] == [6, 8, 10, 12]
+    @test slices[3] == [10, 12, 14, 16]
+    dims(slices[1]) == (Time(1.0:1.0:4.0),)
+    slices = [s .* 2 for s in eachslice(da; dims=Time)] 
+    @test slices[1] == [2, 6, 10]
+    dims(slices[1]) == (Y(10.0:10.0:30.0),)
+end
 
 
 # Dimension reordering methods
