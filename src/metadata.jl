420f549724f7a80d8ac53e171c37c2e31a068c31
Rafael Schouten <rafaelschouten@gmail.com>
2019-12-12 20:30:34 +1100

    dict interface for metadata

--- src/metadata.jl
+++ src/metadata.jl
@@ -1,19 +1,30 @@
 """
 Abstract type for dimension metadata wrappers.
 """
-abstract type Metadata end
+abstract type Metadata{K,V} <: AbstractDict{K,V} end
+
+(::Type{T})() where T <: Metadata = T(Dict())
 
 val(metadata::Metadata) = metadata.val
 
-abstract type AbstractDimMetadata <: Metadata end
+Base.get(metadata::Metadata, args...) = get(val(metadata), args...)
+Base.getindex(metadata::Metadata, args...) = getindex(val(metadata), args...)
+Base.setindex!(metadata::Metadata, args...) = setindex!(val(metadata), args...)
+Base.keys(metadata::Metadata) = keys(val(metadata))
+Base.iterate(metadata::Metadata, args...) = iterate(val(metadata), args...)
+Base.IteratorSize(::Metadata{M})	where M = IteratorSize(M)
+Base.IteratorEltype(::Metadata{M}) where M = IteratorEltype(M)
+Base.eltype(::Metadata{M}) where M = eltype(M)
+Base.length(metadata::Metadata) = length(val(metadata))
 
-abstract type AbstractArrayMetadata <: Metadata end
+abstract type AbstractDimMetadata{K,V} <: Metadata{K,V} end
 
-struct ArrayMetadata{M} <: AbstractArrayMetadata
-    val::M
+abstract type AbstractArrayMetadata{K,V} <: Metadata{K,V} end
+
+struct ArrayMetadata{K,V} <: AbstractArrayMetadata{K,V}
+    val::Dict{K,V}
 end
 
-struct DimMetadata{M} <: AbstractDimMetadata
-    val::M
+struct DimMetadata{K,V} <: AbstractDimMetadata{K,V}
+    val::Dict{K,V}
 end
-
