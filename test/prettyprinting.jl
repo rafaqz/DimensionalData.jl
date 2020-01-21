e4a9b0f2988f2dd0fcd8a0e98c378254153f7548
Datseris <datseris.george@gmail.com>
2020-01-21 18:34:11 +0100

    add tests

--- test/prettyprinting.jl
+++ test/prettyprinting.jl
@@ -1,5 +1,5 @@
 using Dates, DimensionalData
-using DimensionalData: Time, X
+using DimensionalData: Time, X, Y
 timespan = DateTime(2001):Month(1):DateTime(2001,12)
 t = Time(timespan)
 x = X(Vector(10:10:500))
@@ -13,3 +13,14 @@ s3 = sprint(show, MIME("text/plain"), x)
 @test occursin("X", s1)
 @test occursin("X:", s2)
 @test occursin("dimension X", s3)
+
+@test haskey(A, Time)
+@test haskey(A, X)
+@test haskey(A, "Time")
+@test haskey(A, "X")
+@test !haskey(A, Y)
+@test !haskey(A, "Y")
+# @test getkey(A, X) == x # == is not behaving correctly here.
+x2 = getkey(A, X)
+@test x2.val == x.val
+@test_throws ErrorException getkey(A, Y)
