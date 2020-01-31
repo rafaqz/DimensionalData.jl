80648e456f58458f5bd03ba4ae875dafd1fec284
Datseris <datseris.george@gmail.com>
2020-01-31 15:20:35 +0100

    make docs locally buildable in Windows

--- docs/make.jl
+++ docs/make.jl
@@ -1,10 +1,16 @@
 using Documenter, DimensionalData
+CI = get(ENV, "CI", nothing) == "true"
 
 makedocs(
     modules = [DimensionalData],
     sitename = "DimensionalData.jl",
+    format = Documenter.HTML(
+        prettyurls = CI,
+    ),
 )
 
-deploydocs(
-    repo = "github.com/rafaqz/DimensionalData.jl.git",
-)
+if CI
+    deploydocs(
+        repo = "github.com/rafaqz/DimensionalData.jl.git",
+    )
+end
