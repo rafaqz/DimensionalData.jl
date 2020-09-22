edb2dc53911c19b6a0b9fcb6436a58ca47699abc
Rafael Schouten <rafaelschouten@gmail.com>
2020-09-23 00:26:36 +1000

    no shortname

--- test/interface.jl
+++ test/interface.jl
@@ -3,7 +3,5 @@ using DimensionalData: val
 
 @test name(nothing) == ""
 @test name(Nothing) == ""
-@test shortname(nothing) == ""
-@test shortname(Nothing) == ""
 @test dims(1) == nothing
 @test refdims(1) == ()
