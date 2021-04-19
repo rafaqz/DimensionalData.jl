685a4f4442d3750837517e67b5c3b85e5125feb2
Rafael Schouten <rafaelschouten@gmail.com>
2021-04-19 21:51:32 +1000

    regular indexing in Coord

--- test/coord.jl
+++ test/coord.jl
@@ -3,9 +3,20 @@ using DimensionalData, Test
 dim = Coord([(1.0,1.0,1.0), (1.0,2.0,2.0), (3.0,4.0,4.0), (1.0,3.0,4.0)], (X(), Y(), Z()))
 da = DimArray(0.1:0.1:0.4, dim)
 
-@test da[Coord()] === da
-@test da[Coord(:, :, :)] == [0.1, 0.2, 0.3, 0.4]
-@test da[Coord(Between(1, 5), :, At(4.0))] == [0.3, 0.4]
-@test da[Coord(:, Between(1, 3), :)] == [0.1, 0.2]
-@test da[Coord(Z(At(1.0)), Y(Between(1, 3)))] == [0.1]
+@testset "regular indexing" begin
+    @test da[Coord()] === da[Coord(:)] === da
+    @test da[Coord([1, 2])] == [0.1, 0.2]
+    @test da[Coord(4)] == 0.4
+end
+
+@testset "selector indexing" begin
+    @test da[Coord(:, :, :)] == [0.1, 0.2, 0.3, 0.4]
+    @test da[Coord(Between(1, 5), :, At(4.0))] == [0.3, 0.4]
+    @test da[Coord(:, Between(1, 3), :)] == [0.1, 0.2]
+end
+
+@testset "dimension indexing" begin
+    @test da[Coord(Z(At(1.0)), Y(Between(1, 3)))] == [0.1]
+end
+
 @test index(da[Coord(:, Between(1, 3), :)], Coord) == [(1.0,1.0,1.0), (1.0,2.0,2.0)]
