df8c09e3bb380741de2e6899cfd53566a393f49d
Rafael Schouten <rafaelschouten@gmail.com>
2021-04-19 21:30:31 +1000

    dim order Coords

--- test/coord.jl
+++ test/coord.jl
@@ -1,10 +1,11 @@
 using DimensionalData, Test
 
-dim = Coord([(1.0,1.0,1.0), (1.0,2.0,2.0), (3.0,4.0,4.0), (1.0,3.0,4.0)], (X, Y, Z))
+dim = Coord([(1.0,1.0,1.0), (1.0,2.0,2.0), (3.0,4.0,4.0), (1.0,3.0,4.0)], (X(), Y(), Z()))
 da = DimArray(0.1:0.1:0.4, dim)
 
 @test da[Coord()] === da
 @test da[Coord(:, :, :)] == [0.1, 0.2, 0.3, 0.4]
 @test da[Coord(Between(1, 5), :, At(4.0))] == [0.3, 0.4]
 @test da[Coord(:, Between(1, 3), :)] == [0.1, 0.2]
+@test da[Coord(Z(At(1.0)), Y(Between(1, 3)))] == [0.1]
 @test index(da[Coord(:, Between(1, 3), :)], Coord) == [(1.0,1.0,1.0), (1.0,2.0,2.0)]
