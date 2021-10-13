7191d37a6103d37cc9bdf9fa73819fdb2cb8c8c3
rafaqz <rafaelschouten@gmail.com>
2021-10-13 21:06:30 +0200

    test dimindices against cartesian indices

--- test/dimindices.jl
+++ test/dimindices.jl
@@ -2,6 +2,10 @@ using DimensionalData, Test
 
 A = zeros(X(4.0:7.0), Y(10.0:12.0))
 di = DimIndices(A)
+
+ci = CartesianIndices(A)
+@test val.(collect(di)) == Tuple.(collect(ci))
+
 di[4, 3] == (X(4), Y(3))
 @test di[X(1)] == [(Y(1),), (Y(2),), (Y(3),)]
 @test map(ds -> A[ds...] + 2, di) == fill(2.0, 4, 3)
