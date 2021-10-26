0bd43a9b224691ae1e4c172b74de67b2f78ac75b
rafaqz <rafaelschouten@gmail.com>
2021-10-26 23:44:35 +0200

    fix tests

--- test/dimindices.jl
+++ test/dimindices.jl
@@ -7,9 +7,9 @@ ci = CartesianIndices(A)
 @test val.(collect(di)) == Tuple.(collect(ci))
 
 di[4, 3] == (X(4), Y(3))
-@test di[X(1)] == [(Y(1),), (Y(2),), (Y(3),)]
+@test di[X(1)] == [(X(1), Y(1),), (X(1), Y(2),), (X(1), Y(3),)]
 @test map(ds -> A[ds...] + 2, di) == fill(2.0, 4, 3)
-@test map(ds -> A[ds...], di[X(At(7.0))]) == [fill(0.0, 4) for i in 1:3]
+@test map(ds -> A[ds...], di[X(At(7.0))]) == [0.0, 0.0, 0.0]
 @test_throws ArgumentError DimIndices(zeros(2, 2))
 
 @test size(di) == (4, 3)
