117026ae55cf2fd2b72a5be5327c127d8922ec85
George Datseris <datseris.george@gmail.com>
2022-01-04 14:49:39 +0100

    typo missing [

--- src/LookupArrays/LookupArrays.jl
+++ src/LookupArrays/LookupArrays.jl
@@ -4,7 +4,7 @@
 Module for [`LookupArrays`](@ref) and [`Selector`]s used in DimensionalData.jl
 
 `LookupArrays` defines traits and `AbstractArray` wrappers
-that give specific behaviours for a lookup index when indexed with `Selector`](@ref).
+that give specific behaviours for a lookup index when indexed with [`Selector`](@ref).
 
 For example, these allow tracking over array order so fast indexing works evne when 
 the array is reversed.
