a387caec0aee5e37e99eeb714e9a2ae7e6c14e65
rafaqz <rafaelschouten@gmail.com>
2022-08-11 10:14:19 +0000

    use ArrayInterfaceCore for fast using time

--- src/DimensionalData.jl
+++ src/DimensionalData.jl
@@ -21,7 +21,7 @@ using Base: tail, OneTo, @propagate_inbounds
       
 # Ecosystem
 import Adapt, 
-       ArrayInterface,
+       ArrayInterfaceCore,
        ConstructionBase, 
        Extents,
        RecipesBase,
