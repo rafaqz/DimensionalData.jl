543f01df05e8596fb76296838cf77e8fc3008eec
Rafael Schouten <rafaelschouten@gmail.com>
2022-12-13 16:40:48 +0100

    overhaul Coord and rename CoordLookupArray to MergedLookup (#429)
    * overhaul coord
    
    * fix test file name in runtests
    
    * bugfix show_properties
    
    * fix docs
    
    * document and export MergedLookup

---
+++ test/merged.jl
@@ -0,0 +1,63 @@
+using DimensionalData, Test, Unitful
+using DimensionalData.LookupArrays, DimensionalData.Dimensions
+using Statistics: mean
+
+dim = Coord([(1.0,1.0,1.0), (1.0,2.0,2.0), (3.0,4.0,4.0), (1.0,3.0,4.0)], (X(), Y(), Z()))
+da = DimArray(0.1:0.1:0.4, dim)
+da2 = DimArray((0.1:0.1:0.4) * (1:1:3)', (dim, Ti(1u"s":1u"s":3u"s")))
+
+@testset "regular indexing" begin
+    @test da[Coord()] === da[Coord(:)] === da
+    @test da[Coord([1, 2])] == [0.1, 0.2]
+    @test da[Coord(4)] == 0.4
+    @test da2[Coord(4), Ti(3)] ≈ 1.2
+end
+
+@testset "merged selector indexing" begin
+    @test da[Coord(:, :, :)] == [0.1, 0.2, 0.3, 0.4]
+    @test da[Coord(Between(1, 5), :, At(4.0))] == [0.3, 0.4]
+    @test da[Coord(:, Between(1, 2), :)] == [0.1, 0.2]
+    @test da2[Ti(At(1u"s")), Coord(:, Between(1, 2), :)] == [0.1, 0.2]
+    @test da2[Ti(At(2u"s")), Coord(:, Where(>=(3)), :)] == [0.6, 0.8]
+    @test_throws ArgumentError da[var=1, Coord(:, Near(1), :)] 
+end
+
+@testset "merged dimension indexing" begin
+    @test da[Coord(Z(At(1.0)), Y(Between(1, 3)))] == [0.1]
+end
+
+@test index(da[Coord(:, Between(1, 2), :)], Coord) == [(1.0,1.0,1.0), (1.0,2.0,2.0)]
+
+@test bounds(da) == (((1.0, 3.0), (1.0, 4.0), (1.0, 4.0)),)
+
+@testset "merged named reduction" begin
+    m = mean(da2; dims = Coord)
+    @test size(m) == (1,3)
+    @test length(dims(m, Coord)) == 1
+    @test dims(m, Coord).val == DimensionalData.NoLookup(Base.OneTo(1))
+    pure_mean = mean(da2.data; dims = 1)
+    @test vec(pure_mean) == vec(m.data)
+end
+
+@testset "merged indexing with intervals" begin
+    @test da[Coord(Z(At(1.0)), Y(1..3))] == [0.1]
+    @test da2[Ti(At(1u"s")), Coord(X(At(1.0)), Y(At(3.0)), Z(At(4.0)))] == 0.4
+end
+
+@testset "custom merged names" begin
+    A = (1:2) * (1:40)'
+    merged = Dim{:mymerged}(Dimensions.MergedLookup(map(Tuple, vec(CartesianIndices((1:10, 1:4)))), (Dim{:draw}(), Dim{:chain}())))
+    da = DimArray(A, (Dim{:var}(1:2), merged))
+    @test da[var=2, mymerged=(Dim{:draw}(At(8)), Dim{:chain}(At(4)))] == 76
+    @test da[var=1, mymerged=(draw=At(8), chain=At(1))] == 8 
+end
+
+@testset "show merged" begin
+    sprint(show, dim)
+    sp = sprint(show, MIME"text/plain"(), dim)
+    @test occursin("Coord", sp)
+    @test occursin("X, Y, Z", sp)
+    sp = sprint(show, MIME"text/plain"(), da)
+    @test occursin("Coord", sp)
+    @test occursin("X, Y, Z", sp)
+end
