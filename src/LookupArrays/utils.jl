53c63743932a7fd938c52482c25e831061a30e3b
metab0t <metab0t@outlook.com>
2023-02-24 15:21:45 +0800

    Fix typo

--- src/LookupArrays/utils.jl
+++ src/LookupArrays/utils.jl
@@ -3,7 +3,7 @@
 
 Shift the index of `x` from the current locus to the new locus.
 
-We only shift `Samped`, `Regular` or `Explicit`, `Intervals`. 
+We only shift `Sampled`, `Regular` or `Explicit`, `Intervals`. 
 """
 function shiftlocus(locus::Locus, lookup::LookupArray)
     samp = sampling(lookup)
