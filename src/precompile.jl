400db262801ee1cf57433318cb37504841012f49
rafaqz <rafaelschouten@gmail.com>
2023-03-11 14:25:17 +0000

    fix precompile

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,23 +1,24 @@
 
 SnoopPrecompile.@precompile_all_calls begin
     for f in (zeros, ones, falses, trues, rand)
-        A = [1.0 2.0 3.0; 4.0 5.0 6.0]
         x, y, z = X([:a, :b]), Y(10.0:10.0:30.0; metadata=Dict()), Z()
         dimz = x, y
-        f(A) = A[X=1]
+        A = f(x, y)
         da1 = DimArray(A, (x, y); name=:one)
         da2 = DimArray(Float32.(2A), (x, y); name=:two)
-        da3 = DimArray(Int.(3A), (x, y); name=:three)
-        da4 = DimArray(cat(4A, 5A, 6A, 7A; dims=3), (x, y, z); name=:exteradim)
+        da3 = DimArray(round.(Int, 3A), (x, y); name=:three)
+        da4 = DimArray(cat(4A, 5A, 6A, 7A; dims=z); name=:exteradim)
         show(stdout, MIME"text/plain"(), da1)
         show(stdout, MIME"text/plain"(), da2)
         show(stdout, MIME"text/plain"(), da3)
         show(stdout, MIME"text/plain"(), da4)
-        st = DimStack(da1, da2)
-        show(stdout, MIME"text/plain"(), st)
-        st = DimStack(da1, da2, da3)
-        show(stdout, MIME"text/plain"(), st)
-        st = DimStack(da1, da2, da3, da4)
-        show(stdout, MIME"text/plain"(), st)
+        st1 = DimStack(da1, da2)
+        show(stdout, MIME"text/plain"(), st1)
+        st2 = DimStack(da1, da2, da3)
+        show(stdout, MIME"text/plain"(), st2)
+        st3 = DimStack(da1, da2, da3, da4)
+        show(stdout, MIME"text/plain"(), st3)
+        mst1 = merge(st1, st2)
+        mst2 = merge(st1, st3)
     end
 end
