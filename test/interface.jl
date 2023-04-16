b0f93f96ce8b04096eb46c1bec5f4e597e774364
rafaqz <rafaelschouten@gmail.com>
2023-04-16 12:08:19 +0200

    dims on nothing should just return nothing

--- test/interface.jl
+++ test/interface.jl
@@ -3,5 +3,6 @@ using DimensionalData, Test
 @test name(nothing) == ""
 @test name(Nothing) == ""
 @test dims(1) == nothing
+@test dims(nothing) == nothing
 @test_throws ErrorException dims(nothing) 
 @test refdims(1) == ()
