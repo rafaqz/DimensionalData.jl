18176cdd77c50a88c2795e8da95101a98b7a5825
rafaqz <rafaelschouten@gmail.com>
2023-10-22 22:57:05 +0200

    docs

--- src/utils.jl
+++ src/utils.jl
@@ -1,14 +1,29 @@
 
 """
-    reorder(A::AbstractDimArray, order::Pair) => AbstractDimArray
-    reorder(A::Dimension, order::Order) => AbstractDimArray
+    reorder(A::Union{AbstractDimArray,AbstractDimStack}, order::Pair...)
+    reorder(A::Union{AbstractDimArray,AbstractDimStack}, order)
+    reorder(A::Dimension, order::Order)
 
 Reorder every dims index/array to `order`, or reorder index for
-the the given dimension(s) to the `Order` they wrap.
+the the given dimension(s) in `order`.
 
-`order` can be an [`Order`](@ref), or `Dimeension => Order` pairs.
+`order` can be an [`Order`](@ref), `Dimension => Order` pairs.
+A Tuple of Dimensions or any object that defines `dims` can be used
+in which case dimensions are
 
 If no axis reversal is required the same objects will be returned, without allocation.
+
+## Example
+
+```jldoctest
+# Create a DimArray
+da = DimArray([1 2 3; 4 5 6], (X(10:10:20), Y(300:-100:100)))
+# Reverse it
+rev = reverse(da, dims=Y)
+# using `da` in reorder will return it to the original order
+reorder(rev, da) == da
+# output 
+true
 """
 function reorder end
 
