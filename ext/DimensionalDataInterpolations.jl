05d0dada7c9e15486d1e39a611527909440a5ca3
Daniel Boland <dan@deepphase.com>
2024-02-02 12:02:29 -0500

    linear interpolation

--- ext/DimensionalDataInterpolations.jl
+++ ext/DimensionalDataInterpolations.jl
@@ -1,48 +1,62 @@
-module DimensionalDataMakie
+module DimensionalDataInterpolations
 
 using DimensionalData, Interpolations
 using DimensionalData.Dimensions, DimensionalData.LookupArrays
-using Rasters
+# using Rasters
 
-const DD = DimensionalData
+# const DD = DimensionalData
 
-function interp(A::AbstractDimArray; 
-    to=nothing, res=nothing, size=nothing, degree=nothing
-)
-    isempty(otherdims(to, dims(A))) || throw(DimensionMismatch("Cannot interpolate over dimensions not in the source array"))
-    # TODO handle permutations
-    shared_dims = commondims(A, to)
-    # TODO move `_extent2dims` to DimensionalData`
-    dest_dims = dims(Rasters._extent2dims(commondims(to, shared_dims); size, res), shared_dims)
-    other_dims = otherdims(A, to)
-    degrees = if isnothing(degree) 
-        map(_ -> Linear(), dims(A))
-    elseif !(degree isa Tuple)
-        map(_ -> degree, dims(A))
+# function interp(A::AbstractDimArray; 
+#     to=nothing, res=nothing, size=nothing, degree=nothing
+# )
+#     isempty(otherdims(to, dims(A))) || throw(DimensionMismatch("Cannot interpolate over dimensions not in the source array"))
+#     # TODO handle permutations
+#     shared_dims = commondims(A, to)
+#     # TODO move `_extent2dims` to DimensionalData`
+#     dest_dims = dims(Rasters._extent2dims(commondims(to, shared_dims); size, res), shared_dims)
+#     other_dims = otherdims(A, to)
+#     degrees = if isnothing(degree) 
+#         map(_ -> Linear(), dims(A))
+#     elseif !(degree isa Tuple)
+#         map(_ -> degree, dims(A))
+#     else
+#         degree
+#     end
+
+#     data = if isregular(A, (XDim, YDim)) # Do linear interpolation
+#         interpmode = map(dims(A), degrees) do d, deg
+#             BSpline(deg)
+#         end
+#         if isempty(other_dims)
+#             itp = interpolate(A, BSpline(Cubic(Line(OnGrid()))))
+#             sitp = scale(itp, map(parent,  lookup(shared_dims))...)
+#             sitp(map(parent, lookup(dest_dims))...)
+#         else
+#             for D in DimIndices(other_dims)
+#                 itp = interpolate(view(A, D...), BSpline(Cubic(Line(OnGrid()))))
+#                 sitp = scale(itp, map(parent, lookup(shared_dims))...)
+#                 sitp(map(parent, lookup(dest_dims))...)
+#             end
+#         end
+#     else
+#         error("interpolate is only implemented for regular grids")
+#     end
+
+#     return DimArray(data, dest_dims)
+# end
+
+function Interpolations.linear_interpolation(A::AbstractDimArray)
+
+    raw_dims = [dim.val.data for dim in dims(A)]
+
+    isrange(vec) = typeof(vec) <: AbstractRange 
+
+    if all(isrange.(raw_dims))
+        itp = interpolate(A, BSpline(Linear()))
+        sitp = scale(itp, raw_dims...)        
     else
-        degree
+        sitp = interpolate(Tuple(raw_dims),A,Gridded(Linear()))
     end
-
-    data = if isregular(A, (XDim, YDim)) # Do linear interpolation
-        interpmode = map(dims(A), degrees) do d, deg
-            BSpline(deg)
-        end
-        if isempty(other_dims)
-            itp = interpolate(A, BSpline(Cubic(Line(OnGrid()))))
-            sitp = scale(itp, map(parent,  lookup(shared_dims))...)
-            sitp(map(parent, lookup(dest_dims))...)
-        else
-            for D in DimIndices(other_dims)
-                itp = interpolate(view(A, D...), BSpline(Cubic(Line(OnGrid()))))
-                sitp = scale(itp, map(parent, lookup(shared_dims))...)
-                sitp(map(parent, lookup(dest_dims))...)
-            end
-        end
-    else
-        error("interpolate is only implemented for regular grids")
-    end
-
-    return DimArray(data, dest_dims)
 end
 
 end
--- test/interpolations.jl
+++ test/interpolations.jl
@@ -2,6 +2,13 @@ using DimensionalData
 using Interpolations
 
 f((x1, x2)) = log(x1+x2)
-A = f.(DimPoints((X(1:.1:10), Y(1:.5:20))))
+a = f.(DimPoints((X(1:1:10), Y(1:1.5:20))))
+b = f.(DimPoints((X(1:1:10), Y([1.0,3,7.5,10,15]))))
 to = rand(X(2:.3:7), Y(2:.3:17))
-out = DimensionalData.interp(A; to)
+# out = DimensionalData.interp(A; to)
+
+itp_a = linear_interpolation(a)
+itp_b = linear_interpolation(b)
+
+itp_a(7.5,7.5)
+itp_b(7.5,7.5)
