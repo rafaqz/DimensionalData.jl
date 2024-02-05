064d6a3a91addc77325964cdbb6830e5eead5113
Daniel Boland <dan@deepphase.com>
2024-02-05 09:42:37 -0500

    Simpler implementation

--- ext/DimensionalDataInterpolations.jl
+++ ext/DimensionalDataInterpolations.jl
@@ -1,61 +1,13 @@
 module DimensionalDataInterpolations
 
 using DimensionalData, Interpolations
-using DimensionalData.Dimensions, DimensionalData.LookupArrays
-# using Rasters
 
-# const DD = DimensionalData
+function Interpolations.linear_interpolation(A::AbstractDimArray; varargs...)
+    linear_interpolation(DimensionalData.index(dims(A)), A; varargs...)
+end
 
-# function interp(A::AbstractDimArray; 
-#     to=nothing, res=nothing, size=nothing, degree=nothing
-# )
-#     isempty(otherdims(to, dims(A))) || throw(DimensionMismatch("Cannot interpolate over dimensions not in the source array"))
-#     # TODO handle permutations
-#     shared_dims = commondims(A, to)
-#     # TODO move `_extent2dims` to DimensionalData`
-#     dest_dims = dims(Rasters._extent2dims(commondims(to, shared_dims); size, res), shared_dims)
-#     other_dims = otherdims(A, to)
-#     degrees = if isnothing(degree) 
-#         map(_ -> Linear(), dims(A))
-#     elseif !(degree isa Tuple)
-#         map(_ -> degree, dims(A))
-#     else
-#         degree
-#     end
-
-#     data = if isregular(A, (XDim, YDim)) # Do linear interpolation
-#         interpmode = map(dims(A), degrees) do d, deg
-#             BSpline(deg)
-#         end
-#         if isempty(other_dims)
-#             itp = interpolate(A, BSpline(Cubic(Line(OnGrid()))))
-#             sitp = scale(itp, map(parent,  lookup(shared_dims))...)
-#             sitp(map(parent, lookup(dest_dims))...)
-#         else
-#             for D in DimIndices(other_dims)
-#                 itp = interpolate(view(A, D...), BSpline(Cubic(Line(OnGrid()))))
-#                 sitp = scale(itp, map(parent, lookup(shared_dims))...)
-#                 sitp(map(parent, lookup(dest_dims))...)
-#             end
-#         end
-#     else
-#         error("interpolate is only implemented for regular grids")
-#     end
-
-#     return DimArray(data, dest_dims)
-# end
-
-function Interpolations.linear_interpolation(A::AbstractDimArray)
-
-
-    isrange(vec) = typeof(vec) <: AbstractRange 
-
-    if all(DD.isregular, dims(A))
-        itp = interpolate(A, BSpline(Linear()))
-        sitp = scale(itp, DD.index(dims(A))...)        
-    else
-        sitp = interpolate(Tuple(raw_dims),A,Gridded(Linear()))
-    end
+function Interpolations.cubic_spline_interpolation(A::AbstractDimArray; varargs...)
+    cubic_spline_interpolation(DimensionalData.index(dims(A)), A; varargs...)
 end
 
 end
--- test/interpolations.jl
+++ test/interpolations.jl
@@ -9,6 +9,15 @@ to = rand(X(2:.3:7), Y(2:.3:17))
 
 itp_a = linear_interpolation(a)
 itp_b = linear_interpolation(b)
+itp_ex = linear_interpolation(a, extrapolation_bc=(Flat(), Linear()))
 
 itp_a(7.5,7.5)
 itp_b(7.5,7.5)
+itp_ex(10,13)
+itp_ex(11,13)
+itp_ex(10,14)
+
+itp_ca = cubic_spline_interpolation(a)
+# itp_cb = cubic_spline_interpolation(b) #fails, as expected
+
+itp_ca(10,10)
