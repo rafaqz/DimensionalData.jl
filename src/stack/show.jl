15ce1cf742e9bcdcbd4f5b72fbac2baf06037819
rafaqz <rafaelschouten@gmail.com>
2024-02-09 23:41:22 +0100

    more tweaks to show

--- src/stack/show.jl
+++ src/stack/show.jl
@@ -1,20 +1,30 @@
+# Base show
 function Base.summary(io::IO, stack::AbstractDimStack)
     print_ndims(io, size(stack))
     print(io, nameof(typeof(stack)))
 end
 
 function Base.show(io::IO, mime::MIME"text/plain", stack::AbstractDimStack)
-    lines, maxlen, width= print_top(io, mime, stack; bottom_border=false)
-    bottom_border = metadata(stack) isa Union{Nothing,NoMetadata}
-    maxlen = print_layers_block(io, mime, stack; maxlen, width, bottom_border)
-    _, maxlen = print_metadata_block(io, mime, metadata(stack); width, maxlen=min(width-2, maxlen))
-
+    # Show main blocks - summar, dims, layers, metadata
+    _, maxlen = show_main(io, mime, stack)
     # Show anything else subtypes want to append
     show_after(io, mime, stack; maxlen)
     return nothing
 end
 
-function print_layers_block(io, mime, stack; maxlen, width, bottom_border=true)
+# Show customisation interface
+function show_main(io, mime, stack::AbstractDimStack)
+    lines, maxlen, width = print_top(io, mime, stack)
+    maxlen = print_layers_block(io, mime, stack; maxlen, width)
+    _, maxlen = print_metadata_block(io, mime, metadata(stack); width, maxlen=min(width-2, maxlen))
+end
+
+function show_after(io, mime, stack::AbstractDimStack; maxlen)
+    print_block_close(io, maxlen)
+end
+
+# Show blocks
+function print_layers_block(io, mime, stack; maxlen, width)
     layers = DD.layers(stack)
     keylen = if length(keys(layers)) == 0
         0
@@ -25,9 +35,7 @@ function print_layers_block(io, mime, stack; maxlen, width, bottom_border=true)
     for key in keys(layers)
         newmaxlen = min(width - 2, max(maxlen, length(sprint(print_layer, stack, key, keylen))))
     end
-    # Round the corner if this block is larger and sticks out further
-    corner = newmaxlen > maxlen ? '┐' : '┤'
-    printstyled(io, '├', '─'^max(0, newmaxlen - 8), " layers $corner"; color=:light_black)
+    print_block_separator(io, "layers", maxlen, newmaxlen)
     println(io)
     for key in keys(layers)
         print_layer(io, stack, key, keylen)
@@ -38,8 +46,9 @@ end
 function print_layer(io, stack, key, keylen)
     layer = stack[key]
     pkey = rpad(key, keylen)
-    printstyled(io, "  :$pkey", color=dimcolors(100))
-    print(io, string(" ", eltype(layer)))
+    printstyled(io, "  :$pkey", color=dimcolors(7))
+    printstyled(io, " eltype: "; color=:light_black)
+    print(io, string(eltype(layer)))
     field_dims = DD.dims(layer)
     n_dims = length(field_dims)
     colors = map(dimcolors, dimnum(stack, field_dims))
@@ -49,13 +58,10 @@ function print_layer(io, stack, key, keylen)
             Dimensions.print_dimname(IOContext(io, :dimcolor => color), dim)
             i != length(field_dims) && print(io, ", ")
         end
-        print(io, " (")
+        # print(io, " (")
+        printstyled(io, " size: "; color=:light_black)
         print_sizes(io, size(field_dims); colors)
-        print(io, ')')
+        # print(io, ')')
     end
     print(io, '\n')
-end
-
-function show_after(io, mime, stack::DimStack; maxlen)
-    printstyled(io, '└', '─'^maxlen, '┘'; color=:light_black)
 end
