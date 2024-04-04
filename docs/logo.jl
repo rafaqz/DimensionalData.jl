339ac1472027a165b9f49692fe3308447a21cc33
Lazaro Alonso <lazarus.alon@gmail.com>
2024-04-04 22:44:50 +0200

    gen new logo

--- docs/logo.jl
+++ docs/logo.jl
@@ -39,6 +39,6 @@ meshscatter!(ax, base_points; marker=Rect3f(Vec3f(-0.5,-0.5,0), Vec3f(1,1,1)),
     markersize = Vec3f.(0.5,0.5, z), color=z, colormap=tuple.(colors, 1), transparency=false)
 
 mkpath(joinpath(@__DIR__, "src", "assets"))
-save(joinpath(@__DIR__, "src", "assets", "logoDD.svg"), fig; pt_per_unit=0.75)
-save(joinpath(@__DIR__, "src", "assets", "logoDD.png"), fig; px_per_unit=2)
-save(joinpath(@__DIR__, "src", "assets", "favicon.png"), fig; px_per_unit=0.25)
+save(joinpath(@__DIR__, "src", "assets", "logo.svg"), fig; pt_per_unit=0.75)
+save(joinpath(@__DIR__, "src", "assets", "logo.png"), fig; px_per_unit=2)
+save(joinpath(@__DIR__, "src", "assets", "favicon.ico"), fig; px_per_unit=0.25)
