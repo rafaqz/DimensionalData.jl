b2e1aa2af3bc08ca36d436178bdde281322d074e
Felix Cremer <fcremer@bgc-jena.mpg.de>
2024-04-22 16:50:09 +0200

    Small docstring improvements (#698)
    * Improve label docs for Bins and CyclicBins
    
    * Fix typos
    
    * Update src/groupby.jl
    
    ---------
    
    Co-authored-by: Rafael Schouten <rafaelschouten@gmail.com>

--- src/Lookups/Lookups.jl
+++ src/Lookups/Lookups.jl
@@ -6,7 +6,7 @@ Module for [`Lookup`](@ref)s and [`Selector`](@ref)s used in DimensionalData.jl
 `Lookup` defines traits and `AbstractArray` wrappers
 that give specific behaviours for a lookup index when indexed with [`Selector`](@ref).
 
-For example, these allow tracking over array order so fast indexing works evne when 
+For example, these allow tracking over array order so fast indexing works even when 
 the array is reversed.
 
 To load `Lookup` types and methods into scope:
