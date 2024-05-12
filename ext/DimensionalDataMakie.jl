62242d1ed1d888230bb78bbf43cce89c67d8aaa2
Lazaro Alonso <lazarus.alon@gmail.com>
2024-05-12 14:39:39 +0200

    hint, note for volume and surface

--- ext/DimensionalDataMakie.jl
+++ ext/DimensionalDataMakie.jl
@@ -358,6 +358,7 @@ end
 function Makie.convert_arguments(t::SurfaceLikeCompat, A::AbstractDimArray{<:Any,2})
     A1 = _prepare_for_makie(A)
     xs, ys = map(parent, lookup(A1))
+    # the following will not work for irregular spacings, we'll need to add a check for this.
     return xs[1]..xs[end], ys[1]..ys[end], last(Makie.convert_arguments(t, parent(A1)))
 end
 function Makie.convert_arguments(
@@ -370,6 +371,7 @@ end
 function Makie.convert_arguments(t::Makie.VolumeLike, A::AbstractDimArray{<:Any,3}) 
     A1 = _prepare_for_makie(A)
     xs, ys, zs = map(parent, lookup(A1))
+    # the following will not work for irregular spacings
     return xs[1]..xs[end], ys[1]..ys[end], zs[1]..zs[end], last(Makie.convert_arguments(t, parent(A1)))
 end
 # fallbacks with descriptive error messages
