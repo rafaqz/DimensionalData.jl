ea6751adad6a33182854c0bc0c64e5139379939c
JoshuaBillson <jmbillson@outlook.com>
2024-06-20 00:53:05 -0600

    Handle coordinates with different loci

--- src/table_ops.jl
+++ src/table_ops.jl
@@ -37,23 +37,46 @@ function _data_col_names(table, dims::Tuple)
     return filter(x -> !(x in dim_cols), Tables.columnnames(table))
 end
 
-# Determine the ordinality of a set of numerical coordinates
-function _coords_to_ords(coords::AbstractVector, dim::AbstractVector{<:Real})
-    stride = (last(dim) - first(dim)) / (length(dim) - 1)
-    return round.(UInt32, ((coords .- first(dim)) ./ stride) .+ 1)
+# Determine the ordinality of a set of regularly spaced numerical coordinates with a starting locus
+function _coords_to_ords(
+    coords::AbstractVector, 
+    dim::Dimension, 
+    ::Type{<:Real},
+    ::DimensionalData.Start, 
+    ::DimensionalData.Regular)
+    step = (last(dim) - first(dim)) / (length(dim) - 1)
+    return floor.(Int, ((coords .- first(dim)) ./ step) .+ 1)
 end
 
-# Determine the ordinality of a set of categorical coordinates
-function _coords_to_ords(coords::AbstractVector, dim::AbstractVector)
-    d = Dict{eltype(dim),UInt32}()
-    for (i, x) in enumerate(dim)
-        d[x] = i
-    end
-    return map(x -> d[x], coords)
+# Determine the ordinality of a set of regularly spaced numerical coordinates with a central locus
+function _coords_to_ords(
+    coords::AbstractVector, 
+    dim::Dimension, 
+    ::Type{<:Real},
+    ::DimensionalData.Center, 
+    ::DimensionalData.Regular)
+    step = (last(dim) - first(dim)) / (length(dim) - 1)
+    return round.(Int, ((coords .- first(dim)) ./ step) .+ 1)
+end
+
+# Determine the ordinality of a set of regularly spaced numerical coordinates with an end locus
+function _coords_to_ords(
+    coords::AbstractVector, 
+    dim::Dimension, 
+    ::Type{<:Real},
+    ::DimensionalData.End, 
+    ::DimensionalData.Regular)
+    step = (last(dim) - first(dim)) / (length(dim) - 1)
+    return ceil.(Int, ((coords .- first(dim)) ./ step) .+ 1)
+end
+
+# Determine the ordinality of a set of categorical or irregular coordinates
+function _coords_to_ords(coords::AbstractVector, dim::Dimension, ::Any, ::Any, ::Any)
+    return map(c -> DimensionalData.selectindices(dim, At(c)), coords)
 end
 
 # Preprocessing methods for _coords_to_ords
-_coords_to_ords(coords::AbstractVector, dim::Dimension) = _coords_to_ords(coords, collect(dim))
+_coords_to_ords(coords::AbstractVector, dim::Dimension) = _coords_to_ords(coords, dim, eltype(dim), locus(dim), span(dim))
 _coords_to_ords(coords::Tuple, dims::Tuple) = Tuple(_coords_to_ords(c, d) for (c, d) in zip(coords, dims))
 _coords_to_ords(coords::NamedTuple, dims::Tuple) = _coords_to_ords(Tuple(coords[d] for d in name.(dims)), dims)
 
