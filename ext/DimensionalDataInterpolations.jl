cb8e7a21792c1ca0174db0777c6f2349bcd01c5f
Rafael Schouten <rafaelschouten@gmail.com>
2024-06-24 18:17:21 +0200

    Update ext/DimensionalDataInterpolations.jl

--- ext/DimensionalDataInterpolations.jl
+++ ext/DimensionalDataInterpolations.jl
@@ -6,8 +6,8 @@ function Interpolations.linear_interpolation(A::AbstractDimArray; kw...)
     linear_interpolation(DimensionalData.index(dims(A)), A; kw...)
 end
 
-function Interpolations.cubic_spline_interpolation(A::AbstractDimArray; varargs...)
-    cubic_spline_interpolation(DimensionalData.index(dims(A)), A; varargs...)
+function Interpolations.cubic_spline_interpolation(A::AbstractDimArray; kw...)
+    cubic_spline_interpolation(DimensionalData.index(dims(A)), A; kw...)
 end
 
 end
