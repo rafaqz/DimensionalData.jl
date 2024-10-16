1c4ffe1321165eb8adda1b4286668cb161916c57
Anshul Singhvi <anshulsinghvi@gmail.com>
2024-10-16 11:27:05 -0700

    Allow types and constructed dims
    Co-authored-by: Rafael Schouten <rafaelschouten@gmail.com>

--- ext/DimensionalDataAlgebraOfGraphicsExt.jl
+++ ext/DimensionalDataAlgebraOfGraphicsExt.jl
@@ -14,7 +14,7 @@ objects, which is the type that `AlgebraOfGraphics.data` returns.
 =#
 
 # The generic selector, to enable this to work even in `DataFrame(dimarray)`
-function AoG.select(data::AoG.Columns, dim::Type{<: DD.Dimensions.Dimension})
+function AoG.select(data::AoG.Columns, dim::DD.Dimensions.DimOrDimType)
     name = DD.name(dim)
     v = AoG.getcolumn(data.columns, Symbol(name))
     return (v,) => identity => AoG.to_string(name) => nothing
@@ -23,7 +23,7 @@ end
 # The specific selector for `DimTable`s.
 # This searches the dimensions of the dimtable for the appropriate dimension,
 # so that e.g. `X` also applies to any `XDim`, and so forth.
-function AoG.select(data::AoG.Columns{<: DD.DimTable}, dim::Type{<: DD.Dimensions.Dimension})
+function AoG.select(data::AoG.Columns{<: DD.DimTable}, dim::DD.Dimensions.DimOrDimType)
     available_dimension = DD.dims(data.columns, dim)
     name = DD.name(available_dimension)
     v = AoG.getcolumn(data.columns, Symbol(name))
