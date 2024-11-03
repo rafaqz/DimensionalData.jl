93ad378d2fa2f505264d479a5c05315a58fce2e9
Rafael Schouten <rafaelschouten@gmail.com>
2024-11-04 00:57:18 +0100

    check AbstractVector{Int} index sorted order

--- src/Lookups/indexing.jl
+++ src/Lookups/indexing.jl
@@ -8,7 +8,10 @@ for f in (:getindex, :view, :dotview)
         @propagate_inbounds Base.$f(l::Lookup, i::Union{AbstractVector,Colon}) = 
             rebuild(l; data=Base.$f(parent(l), i))
         @propagate_inbounds function Base.$f(l::Union{Sampled,Categorical}, i::AbstractVector{Int})
-            rebuild(l; data=Base.$f(parent(l), i), order=Unordered())
+            if isordered(l)
+                issorted(i) || throw(ArgumentError("For `ForwardOrdered` or `ReverseOrdered` lookups, indices of `AbstractVector{Int}` must be in ascending order"))
+            end
+            rebuild(l; data=Base.$f(parent(l), i))
         end
         # Selector gets processed with `selectindices`
         @propagate_inbounds Base.$f(l::Lookup, i::SelectorOrInterval) = Base.$f(l, selectindices(l, i))
