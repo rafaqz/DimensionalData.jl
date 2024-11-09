b73b6ee223ad59a5ba829ad61241d0c1484d3bf1
Anshul Singhvi <anshulsinghvi@gmail.com>
2024-11-09 14:39:31 -0500

    allow `DiskArrays.cache` to operate on the `parent` of a dimarray. (#849)
    * Add a DiskArrays extension to DimensionalData
    
    * Implement `cache`, `haschunks`, `eachchunk` for DimArrays
    
    basically everything forwards to the parent array, and `chunk` rebuilds the dimarray with the cached parent.
    
    * Add tests
    
    * Fix tests
    
    * Add docs
    
    * Update ext/DimensionalDataDiskArraysExt.jl
    
    Co-authored-by: Rafael Schouten <rafaelschouten@gmail.com>
    
    * Update ext/DimensionalDataDiskArraysExt.jl
    
    * Update ext/DimensionalDataDiskArraysExt.jl
    
    ---------
    
    Co-authored-by: Rafael Schouten <rafaelschouten@gmail.com>

---
+++ ext/DimensionalDataDiskArraysExt.jl
@@ -0,0 +1,19 @@
+"""
+    DimensionalDataDiskArraysExt
+
+Extend some methods of DiskArrays (`cache`, etc) to work on the base data of any DimArray.  
+"""
+module DimensionalDataDiskArraysExt
+
+using DimensionalData
+import DimensionalData: AbstractBasicDimArray
+import DiskArrays
+
+DiskArrays.cache(x::Union{AbstractDimStack,AbstractDimArray}; kw...) = 
+    modify(A -> DiskArrays.cache(A; kw...), x)
+
+DiskArrays.haschunks(da::AbstractBasicDimArray) = DiskArrays.haschunks(parent(da))
+DiskArrays.eachchunk(da::AbstractBasicDimArray) = DiskArrays.eachchunk(parent(da))
+
+
+end
