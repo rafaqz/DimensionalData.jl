01493188bab96f6ae7bf11af7a553da3e152007c
Anshul Singhvi <anshulsinghvi@gmail.com>
2024-11-26 15:38:08 -0500

    Update DimensionalDataDiskArraysExt.jl

--- ext/DimensionalDataDiskArraysExt.jl
+++ ext/DimensionalDataDiskArraysExt.jl
@@ -9,8 +9,13 @@ using DimensionalData
 import DimensionalData: AbstractBasicDimArray
 import DiskArrays
 
-DiskArrays.cache(x::Union{AbstractDimStack,AbstractDimArray}; kw...) = 
-    modify(A -> DiskArrays.cache(A; kw...), x)
+# cache was only introduced in DiskArrays v0.4, so
+# we lock out the method definition if the method does
+# not exist.
+@static if :cache in names(DiskArrays)
+    DiskArrays.cache(x::Union{AbstractDimStack,AbstractDimArray}; kw...) = 
+        modify(A -> DiskArrays.cache(A; kw...), x)
+end
 
 DiskArrays.haschunks(da::AbstractBasicDimArray) = DiskArrays.haschunks(parent(da))
 DiskArrays.eachchunk(da::AbstractBasicDimArray) = DiskArrays.eachchunk(parent(da))
