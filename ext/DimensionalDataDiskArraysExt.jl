6d0c9f3406830a89d661b645638097595e612e08
Rafael Schouten <rafaelschouten@gmail.com>
2025-02-20 17:08:57 +0100

    bugfix pad

--- ext/DimensionalDataDiskArraysExt.jl
+++ ext/DimensionalDataDiskArraysExt.jl
@@ -9,6 +9,8 @@ using DimensionalData
 import DimensionalData: AbstractBasicDimArray
 import DiskArrays
 
+const DD = DimensionalData
+
 DiskArrays.haschunks(da::AbstractBasicDimArray) = DiskArrays.haschunks(parent(da))
 DiskArrays.eachchunk(da::AbstractBasicDimArray) = DiskArrays.eachchunk(parent(da))
 
@@ -29,32 +31,38 @@ end
     DiskArrays.pad(x::AbstractDimArray, padding::Tuple{Vararg{Tuple{<:Integer,<:Integer}}}; kw...) =
         DiskArrays.pad(x, map(rebbuild, dims(x, padding)))
     DiskArrays.pad(x::AbstractDimArray, padding::NamedTuple; kw...) =
-        DiskArrays.pad(x, DimensionalData.kw2dims(padding); kw...)
-    DiskArrays.pad(x::AbstractDimArray, pairs::Pair...; kw...) =
-        DiskArrays.pad(x, DimensionalData.pair2dims(pairs); kw...)
-    function DiskArrays.pad(x::AbstractDimArray, padding::DimTuple; kw...)
-        tuple_padding = map(dims(x)) do d
-            hasdim(padding, d) ? map(Int, val(dim(padding, d))) : (0, 0)
+        DiskArrays.pad(x, DD.kw2dims(padding); kw...)
+    DiskArrays.pad(x::AbstractDimArray, p1::Pair, pairs::Pair...; kw...) =
+        DiskArrays.pad(x, DD.Dimensions.pairs2dims(p1, pairs...); kw...)
+    function DiskArrays.pad(x::AbstractDimArray, padding::DD.DimTuple; kw...)
+        tuple_padding = map(DD.dims(x)) do d
+            hasdim(padding, d) ? map(Int, val(DD.dims(padding, d))) : (0, 0)
         end
-        dims = map(padding) do dp
-            DiskArrays.pad(dims(x, dp), val(dp))
+        paddeddims = map(DD.dims(x)) do d
+            if hasdim(padding, d) 
+                DiskArrays.pad(d, val(DD.dims(padding, d)))
+            else
+                d
+            end
         end
-        data = DiskArrays.pad(parent(x), tuple_padding; kw...)
-        return rebuild(x; data, dims)
+        paddeddata = DiskArrays.pad(parent(x), tuple_padding; kw...)
+        return rebuild(x; data=paddeddata, dims=paddeddims)
     end
-    DiskArrays.pad(d::Dimension, padding::Tuple{<:Integer,<:Integer}) = 
-        DiskArrays.pad(lookup(d), padding)
-    function DiskArrays.pad(l::Lookup, padding::Tuple{<:Integer,<:Integer})
-        isregular(l) || throw(ArgumentError("Can only pad `Regular` lookups"))
+    DiskArrays.pad(d::DD.Dimension, padding::Tuple{<:Integer,<:Integer}) = 
+        rebuild(d, DiskArrays.pad(lookup(d), padding))
+    DiskArrays.pad(l::DD.AbstractNoLookup, (startpad, stoppad)::Tuple{<:Integer,<:Integer}) =
+        NoLookup(Base.OneTo(length(l) + startpad + stoppad))
+    function DiskArrays.pad(l::DD.Lookup, padding::Tuple{<:Integer,<:Integer})
+        (DD.issampled(l) && DD.isregular(l)) || throw(ArgumentError("Can only pad `Regular` `AbstractSampled` lookups"))
         rebuild(l; data=_pad(parent(l), step(l), padding))
         # Use ranges for math because they have TwicePrecision magic
         # Define a range down to the lowest value,
         # but anchored at the existing value
     end
 
-    _pad(l::AbstractRange, step, (startpad, stoppad)::Tuple{<:Integer,<:Integer})
+    _pad(l::AbstractRange, step, (startpad, stoppad)::Tuple{<:Integer,<:Integer}) =
         (first(l) - startpad * step(l)):step(l):(last(l) + stoppad * step(l))
-    _pad(l::AbstractUnitRange, step, (startpad, stoppad)::Tuple{<:Integer,<:Integer})
+    _pad(l::AbstractUnitRange, step, (startpad, stoppad)::Tuple{<:Integer,<:Integer}) =
         (first(l) - startpad):(last(l) + stoppad)
     function _pad(l::AbstractVector, step, (startpad, stoppad)::Tuple{<:Integer,<:Integer})
         # First pad as a range
