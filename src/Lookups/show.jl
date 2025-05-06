ae5eee9e8e8cdbbcde41a35a3c7604ff916b12d6
Rafael Schouten <rafaelschouten@gmail.com>
2025-05-06 03:10:28 +0200

    clean up show

--- src/Lookups/show.jl
+++ src/Lookups/show.jl
@@ -24,7 +24,7 @@ end
 
 function Base.show(io::IO, mime::MIME"text/plain", lookup::Lookup)
     show_compact(io, mime, lookup)
-    get(io, :compact, false) && print_index(io, mime, parent(lookup))
+    get(io, :compact, false) && print_lookup_values(io, mime, parent(lookup))
     show_properties(io, mime, lookup)
     if !get(io, :compact, false) 
         println(io)
@@ -90,18 +90,15 @@ function print_metadata(io, lookup)
     print(io, nameof(typeof(metadata(lookup))))
 end
 
-function print_index(io, mime, A::AbstractRange, nchars=0)
+function print_lookup_values(io, mime, A::AbstractRange, nchars=0)
     print(io, " ")
     printstyled(io, repr(A); color=get(io, :dimcolor, :white))
 end
-function print_index(io, mime, v::AbstractVector, nchars=0)
+function print_lookup_values(io, mime, v::AbstractVector, nchars=0)
     print(io, " ")
-    # Maximum 2 values for dates
-    vals = if length(v) > 2 && eltype(v) <: Dates.TimeType
-        "$(v[begin]), …, $(v[end])"
-    # Maximum 4 values for other types 
-    elseif length(v) > 5
-        "$(v[begin]), $(v[begin+1]), …, $(v[end-1]), $(v[end])"
+    # Maximum 2 values
+    vals = if length(v) > 2
+        join((repr(v[begin]), "…", repr(v[end])), ", ")
     else
         join((repr(va) for va in v), ", ")
     end
