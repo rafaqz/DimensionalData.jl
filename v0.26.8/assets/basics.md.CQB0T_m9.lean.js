6cb8d63ed875ee33fb97962d3f8f567fb89315f9
Documenter.jl <documenter@juliadocs.github.io>
2025-08-24 01:05:06 +0000

    delete history

---
+++ v0.26.8/assets/basics.md.CQB0T_m9.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as i,o as a,a6 as n}from"./chunks/framework.Damosf4w.js";const c=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"basics.md","filePath":"basics.md","lastUpdated":null}'),h={name:"basics.md"},l=n("",21),k=[l];function t(e,p,d,r,g,y){return a(),i("div",null,k)}const E=s(h,[["render",t]]);export{c as __pageData,E as default};
