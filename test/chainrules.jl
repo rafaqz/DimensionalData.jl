5295f0192a9db975f1c8a032c7bd2628983e1b4b
Lazaro Alonso <lazarus.alon@gmail.com>
2025-09-15 10:26:28 +0200

    adds selectors rrule (#1096)
    * adds selectors rrule
    
    * use dims2indices

--- test/chainrules.jl
+++ test/chainrules.jl
@@ -50,5 +50,28 @@ using ChainRulesCore
         _, Āz = pb(NoTangent())
         @test Āz === NoTangent()
     end
+    @testset "getindex rrule with selectors" begin
+        A = DimArray(rand(3, 4), (Y([:a, :b, :c]), X(1:4)))
+        
+        # Test At selector
+        y, pb = rrule(getindex, A; Y=At(:b))
+        @test size(y) == (4,)  # Only X dimension remains
+        
+        ȳ = ones(4)
+        _, grad = pb(ȳ)
+        @test size(grad) == size(A)
+        @test all(grad[1, :] .== 0)
+        @test all(grad[2, :] .== 1)  # Y=:b 
+        @test all(grad[3, :] .== 0)
+        
+        # Test multiple selectors
+        y2, pb2 = rrule(getindex, A; Y=At(:b), X=At(2))
+        @test y2 isa Number
+        
+        ȳ2 = 1.0
+        _, grad2 = pb2(ȳ2)
+        @test grad2[2, 2] == 1  # Y=:b, X=2
+        @test sum(grad2) == 1   # Only one element should be 1
+    end
 
 end
